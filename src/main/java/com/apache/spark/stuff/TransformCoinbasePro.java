c93bad92ab5eeaf7bfcec4fb5534bfd1f090d31e
larry adams <ladams1776@protonmail.com>
2021-07-18 16:20:14 -0400

    Add innards for Coinbase Pro transformation

--- src/main/java/com/apache/spark/stuff/TransformCoinbasePro.java
+++ src/main/java/com/apache/spark/stuff/TransformCoinbasePro.java
@@ -1,6 +1,13 @@
 package com.apache.spark.stuff;
 
-import com.apache.spark.stuff.util.JsonMultilineReaderFactory;
+import static org.apache.spark.sql.functions.abs;
+import static org.apache.spark.sql.functions.col;
+import static org.apache.spark.sql.functions.lit;
+import static org.apache.spark.sql.functions.to_date;
+import static org.apache.spark.sql.functions.to_timestamp;
+import static org.apache.spark.sql.functions.unix_timestamp;
+
+import com.apache.spark.stuff.util.CsvReaderFactory;
 import com.apache.spark.stuff.util.SparkSessionFactory;
 import com.apache.spark.stuff.util.WriterFactory;
 import org.apache.log4j.Level;
@@ -9,6 +16,9 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
+/**
+ * Transform Coinbase Pro spreadsheet into columns I can use for the other app
+ */
 public class TransformCoinbasePro {
 
   public static void main(String[] args) {
@@ -17,20 +27,27 @@ public class TransformCoinbasePro {
 
     // Declare everything
     final SparkSessionFactory sparkSessionFactory = new SparkSessionFactory();
-    final JsonMultilineReaderFactory jsonMultilineReaderFactory = new JsonMultilineReaderFactory();
+    final CsvReaderFactory csvReaderFactory = new CsvReaderFactory();
     final WriterFactory writerFactory = new WriterFactory();
 
     // Setup
     final SparkSession sparkSession = sparkSessionFactory.get();
-    final Dataset<Row> originalDataset = jsonMultilineReaderFactory.apply(sparkSession, args[0]);
+    final Dataset<Row> originalDataset = csvReaderFactory.apply(sparkSession, args[0]);
 
-    // product - Coin
-    // side - Activity
-    // created at - Time
-    // size - Amount
-    // price - PricePerCoin
-    // abs(total) - Amount
-    // unix_timestamp(created at) - Unix
-    // date_format(create at) - Date
+//    writerFactory.accept(originalDataset
+    originalDataset.withColumn("Prep Columns ->", lit("----->"))
+        .withColumn("created_date", to_date(col("created at")))
+        .withColumn("Starting here ->", lit("--------------->"))
+        .withColumn("Activity", col("side"))
+        .withColumn("PricePerCoin", col("price"))
+        .withColumn("Time", col("created_date"))
+        .withColumn("Date", to_timestamp(col("created_date")))
+        .withColumn("Unix", unix_timestamp(col("created_date")))
+        .withColumn("Order", abs(col("total")))
+        .withColumn("Amount", col("size"))
+        .withColumn("Coin", col("size unit"))
+        .withColumn("Source", lit("Coinbase Pro"))
+        .show();
+//  );
   }
 }
