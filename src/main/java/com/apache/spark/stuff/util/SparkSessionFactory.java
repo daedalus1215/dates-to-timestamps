02e71795ca923235ebc677a7eb1e46bf70fc1ec8
la <lfadms@yahoo.com>
2021-07-17 23:50:47 -0400

    Create SparkSessionFactory

---
+++ src/main/java/com/apache/spark/stuff/util/SparkSessionFactory.java
@@ -0,0 +1,22 @@
+package com.apache.spark.stuff.util;
+
+import java.util.function.Supplier;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
+
+public class SparkSessionFactory implements Supplier<SparkSession> {
+
+  @Override
+  public SparkSession get() {
+    SparkConf conf = new SparkConf()
+        .setAppName("startingSpark")
+        .setMaster("local[*]");
+    JavaSparkContext sc = new JavaSparkContext(conf);
+    return SparkSession.builder()
+        .appName("testingSql") // name we see this in a profiler
+        .master("local[*]") // how many cores
+        .config("spark.sql.warehouse.dir", "file:///d:/tmp/") // required
+        .getOrCreate();
+  }
+}
