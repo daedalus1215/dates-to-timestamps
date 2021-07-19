a9456fc05359631a44ecaf973f00c9b2b1e2109e
larry adams <ladams1776@protonmail.com>
2021-07-18 20:51:49 -0400

    Update TransformCoinbasePro to get the date correct.

--- src/main/java/com/apache/spark/stuff/TransformCoinbasePro.java
+++ src/main/java/com/apache/spark/stuff/TransformCoinbasePro.java
@@ -3,6 +3,7 @@ package com.apache.spark.stuff;
 import static org.apache.spark.sql.functions.abs;
 import static org.apache.spark.sql.functions.col;
 import static org.apache.spark.sql.functions.lit;
+import static org.apache.spark.sql.functions.regexp_replace;
 import static org.apache.spark.sql.functions.to_date;
 import static org.apache.spark.sql.functions.to_timestamp;
 import static org.apache.spark.sql.functions.unix_timestamp;
@@ -34,20 +35,20 @@ public class TransformCoinbasePro {
     final SparkSession sparkSession = sparkSessionFactory.get();
     final Dataset<Row> originalDataset = csvReaderFactory.apply(sparkSession, args[0]);
 
-//    writerFactory.accept(originalDataset
-    originalDataset.withColumn("Prep Columns ->", lit("----->"))
+    writerFactory.accept(originalDataset
+    .withColumn("Prep Columns ->", lit("----->"))
         .withColumn("created_date", to_date(col("created at")))
         .withColumn("Starting here ->", lit("--------------->"))
         .withColumn("Activity", col("side"))
         .withColumn("PricePerCoin", col("price"))
         .withColumn("Time", col("created_date"))
-        .withColumn("Date", to_timestamp(col("created_date")))
+        .withColumn("Date", regexp_replace(to_timestamp(col("created_date")), "T", " "))
         .withColumn("Unix", unix_timestamp(col("created_date")))
         .withColumn("Order", abs(col("total")))
         .withColumn("Amount", col("size"))
         .withColumn("Coin", col("size unit"))
         .withColumn("Source", lit("Coinbase Pro"))
-        .show();
-//  );
+//        .show();
+  );
   }
 }
