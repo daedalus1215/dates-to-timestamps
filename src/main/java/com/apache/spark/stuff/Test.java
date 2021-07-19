ae8e76095fd063e05d4f1b48dc8d4163ba4e5a16
larry adams <ladams1776@protonmail.com>
2021-07-18 23:21:12 -0400

    Update Test to unionize another file to create more rows.

--- src/main/java/com/apache/spark/stuff/Test.java
+++ src/main/java/com/apache/spark/stuff/Test.java
@@ -1,19 +1,17 @@
 package com.apache.spark.stuff;
 
+import static org.apache.spark.sql.functions.avg;
 import static org.apache.spark.sql.functions.col;
 import static org.apache.spark.sql.functions.count;
-import static org.apache.spark.sql.functions.date_format;
 import static org.apache.spark.sql.functions.lit;
-import static org.apache.spark.sql.functions.sum;
 import static org.apache.spark.sql.functions.to_date;
 
-import com.apache.spark.stuff.util.SparkSessionFactory;
 import com.apache.spark.stuff.util.JsonMultilineReaderFactory;
+import com.apache.spark.stuff.util.SparkSessionFactory;
 import com.apache.spark.stuff.util.WriterFactory;
 import com.sun.prism.PixelFormat.DataType;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
@@ -30,33 +28,31 @@ public class Test {
     final JsonMultilineReaderFactory jsonMultilineReaderFactory = new JsonMultilineReaderFactory();
     final WriterFactory writerFactory = new WriterFactory();
 
-    // Setup
     final SparkSession sparkSession = sparkSessionFactory.get();
-    final Dataset<Row> originalDataset = jsonMultilineReaderFactory.apply(sparkSession, args[0]);
 
-    final Dataset<Row> agg = originalDataset
-        .withColumn("wattz", col("watts").cast(String.valueOf(DataType.INT)))
-        .withColumn("date_no_time", to_date(col("date")))
-        .groupBy(col("DeviceHost"), col("date_no_time"))
-        .agg(
-            count(lit(1)).alias("Num_Of_Records"),
-            sum("wattz").alias("summed_watts"));
+    // Setup
+    final Dataset<Row> ravencoinRig = jsonMultilineReaderFactory.apply(sparkSession, args[0])
+        .withColumn("Day", to_date(col("date")));
+
+    final Dataset<Row> bagginsRig = jsonMultilineReaderFactory.apply(sparkSession, args[1])
+        .withColumn("Day", to_date(col("date")));
+
+    final Dataset<Row> combined = ravencoinRig.union(bagginsRig);
+
+    final Dataset<Row> agg = combined
+        .withColumnRenamed("name", "Name")
+        .withColumn("Wattz", col("watts").cast(String.valueOf(DataType.INT)))
+        .groupBy(col("Day"), col("name"))
+        .agg(count(lit(1)).alias("Number of Log Entries"),
+            avg("Wattz").alias("Average Watts an Hour"));
 
     final Dataset<Row> rowDataset = agg
-        .withColumn("energy_for_day",
-            col("summed_watts").divide(col("Num_Of_Records")).divide(1000))
-        .withColumn("cost_for_day", col("energy_for_day").multiply(0.07));
+        .withColumn("Energy Consumption for Day", col("Average Watts an Hour").divide(1000))
+        .withColumn("Energy Cost for Day", col("Energy Consumption for Day").multiply(0.07));
 
-    writerFactory.accept(rowDataset);
+//    writerFactory.accept(rowDataset);
+    rowDataset.show();
 
     sparkSession.close();
-  }
-
-  private static Dataset<Row> getAverage(Dataset<Row> ds) {
-    final Column dateFormat = date_format((col("date")), "yyyy-MM-dd HH:mm:ss");
-    return ds
-        .withColumn("check", dateFormat)
-        .withColumn("wattz", col("watts").cast(String.valueOf(DataType.INT)))
-        .groupBy(col("DeviceHost")).avg("wattz");
   }
 }
