74ba70f612c71633777aaaf621a28a8d0452ac8c
larry adams <ladams1776@protonmail.com>
2021-07-20 22:38:27 -0400

    Adding more, I think it is correct atm. Cross checked with website.

--- src/main/java/com/apache/spark/stuff/TpEnergyLinkApp.java
+++ src/main/java/com/apache/spark/stuff/TpEnergyLinkApp.java
@@ -5,7 +5,9 @@ import static org.apache.spark.sql.functions.col;
 import static org.apache.spark.sql.functions.count;
 import static org.apache.spark.sql.functions.date_format;
 import static org.apache.spark.sql.functions.lit;
+import static org.apache.spark.sql.functions.month;
 import static org.apache.spark.sql.functions.to_date;
+import static org.apache.spark.sql.functions.year;
 
 import com.apache.spark.stuff.util.JsonMultilineReaderFactory;
 import com.apache.spark.stuff.util.SparkSessionFactory;
@@ -34,26 +36,37 @@ public class TpEnergyLinkApp {
 
     // Setup
     final Dataset<Row> testing = jsonMultilineReaderFactory.apply(sparkSession, args[0])
-//        .withColumn("Date", unix_timestamp(col("ts"), "MM-dd-yyyy HH:mm:ss"))
-        .withColumn("Date",
-            date_format(col("ts").divide(1000).cast(DataTypes.TimestampType), "yyyy-MM-dd"))
-        .withColumn("Day", to_date(col("Date")));
+        .withColumn("Name", lit(args[0]))
+        .withColumn("Date", date_format(col("ts").divide(1000).cast(DataTypes.TimestampType),
+            "yyyy-MM-dd HH:mm:ss"))
+        .withColumn("Day", to_date(col("Date")))
+        .withColumn("Month", month(col("Date")))
+        .withColumn("Year", year(col("Date")));
 
-    final Dataset<Row> combined = testing;
-//        .union(bagginsRig)
+    final Dataset<Row> testing2 = jsonMultilineReaderFactory.apply(sparkSession, args[1])
+        .withColumn("Name", lit(args[1]))
+        .withColumn("Date", date_format(col("ts").divide(1000).cast(DataTypes.TimestampType),
+            "yyyy-MM-dd HH:mm:ss"))
+        .withColumn("Day", to_date(col("Date")))
+        .withColumn("Month", month(col("Date")))
+        .withColumn("Year", year(col("Date")));
+
+    final Dataset<Row> combined = testing
+        .union(testing2);
 //        .union(fiveNintyAndFiftySevenHundred);
 
     final Dataset<Row> agg = combined
-//        .withColumnRenamed("name", "Name")
         .withColumn("Wattz", col("pw").cast(String.valueOf(DataType.INT)))
-        .groupBy(col("Day"), col("Date"))
+        .groupBy(col("Month"), col("Year"), col("Name"))
         .agg(count(lit(1)).alias("Number of Log Entries"),
             avg("Wattz").alias("Average Watts an Hour"));
 
     //@TODO: Doublecheck these numbers
     final Dataset<Row> rowDataset = agg
         .withColumn("Energy Consumption for Day", col("Average Watts an Hour").divide(1000))
-        .withColumn("Energy Cost for Day", col("Energy Consumption for Day").multiply(0.07));
+        .withColumn("Energy Cost for Day",
+            col("Energy Consumption for Day").multiply(0.07).multiply(24))
+        .withColumn("Expected Monthly Cost", col("Energy Cost for Day").multiply(30));
 
 //    writerFactory.accept(rowDataset);
     rowDataset.show();
