8263802a53868ec0cb43d0a524d38dc313c2274d
larry adams <ladams1776@protonmail.com>
2021-07-24 23:53:17 -0400

    Update WriterFactory allow more dynamic writing

--- src/main/java/com/apache/spark/stuff/util/WriterFactory.java
+++ src/main/java/com/apache/spark/stuff/util/WriterFactory.java
@@ -1,16 +1,20 @@
 package com.apache.spark.stuff.util;
 
-import java.util.function.Consumer;
+import java.util.function.BiConsumer;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 
-public class WriterFactory implements Consumer<Dataset<Row>> {
+public class WriterFactory implements BiConsumer<Dataset<Row>, String> {
+
+  private final String RESOURCES_DIRECTORY = "src/main/resources/";
 
   @Override
-  public void accept(Dataset<Row> rowDataset) {
+  public void accept(Dataset<Row> rowDataset, String subPath) {
     rowDataset.write()
         .option("header", "true")
         .mode("overwrite")
-        .csv("src/main/resources/target.csv");
+        .csv(RESOURCES_DIRECTORY
+            .concat(subPath)
+            .concat("/"));
   }
 }
