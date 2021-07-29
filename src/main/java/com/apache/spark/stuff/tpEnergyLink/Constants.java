45ecc6766b49f44e3d1f583a1dee566d0871b9bb
larry adams <ladams1776@protonmail.com>
2021-07-28 22:48:41 -0400

    Adding more to the Constants

---
+++ src/main/java/com/apache/spark/stuff/tpEnergyLink/Constants.java
@@ -0,0 +1,26 @@
+package com.apache.spark.stuff.tpEnergyLink;
+
+public class Constants {
+  // Join
+  public static String LEFT_JOIN = "left";
+
+  // Energy Source Files
+  public static String SOURCE_POWER = "pw";
+  public static String SOURCE_UNIX_TIMESTAMP = "ts";
+
+  public static String CREATED_ID = "id_TEMP";
+  public static String CREATED_REMOVED_PATH_TO_FILE_TEMP = "REMOVED_PATH_TO_FILE_TEMP";
+  public static String CREATED_FILE_NAME_AND_PREFIX_TEMP = "FILE_NAME_AND_PREFIX_TEMP";
+  public static String CREATED_FILE_NAME_AND_PARENT_DIR_TEMP = "FILE_NAME_AND_PARENT_DIR_TEMP";
+  public static String CREATED_SUM_WATTS_FOR_DAY_AND_PLUG_TEMP = "CREATED_SUM_WATTS_FOR_DAY_AND_PLUG_TEMP";
+
+  // Plugin File
+
+  // Assets
+  public static String RVN_SOURCE_ID = "RIG_ID";
+  public static String RVN_SOURCE_DATE = "Date";
+  public static String RVN_CREATED_DATE = "RVN_CREATED_DATE";
+  public static String RVN_CREATED_COUNT_OF_RECORDS_A_DAY = "RVN_CREATED_COUNT_OF_RECORDS_A_DAY_FOR_RVN";
+  public static String RVN_CREATED_SUM_OF_A_DAY = "RVN_CREATED_SUM_OF_RVN_A_DAY";
+  public static String RVN_SOURCE_AMOUNT = "Amount (RVN)";
+}
