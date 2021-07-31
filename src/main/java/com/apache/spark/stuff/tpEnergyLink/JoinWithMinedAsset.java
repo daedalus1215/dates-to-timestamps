e9f11d6a0674561e9571b6f9cfb5da65603df110
larry adams <ladams1776@protonmail.com>
2021-07-31 11:31:57 -0400

    Remove show and clean up JoinWithMinedAsset

--- src/main/java/com/apache/spark/stuff/tpEnergyLink/JoinWithMinedAsset.java
+++ src/main/java/com/apache/spark/stuff/tpEnergyLink/JoinWithMinedAsset.java
@@ -1,7 +1,8 @@
 package com.apache.spark.stuff.tpEnergyLink;
 
-import static com.apache.spark.stuff.tpEnergyLink.Constants.LEFT_JOIN;
-import static com.apache.spark.stuff.tpEnergyLink.Constants.RVN_CREATED_DATE;
+import static com.apache.spark.stuff.tpEnergyLink.Constants.CREATED_UNIQUE_ID;
+import static com.apache.spark.stuff.tpEnergyLink.Constants.OUTER_JOIN;
+import static com.apache.spark.stuff.tpEnergyLink.Constants.RVN_CREATED_UNIQUE_ID;
 
 import java.util.function.BiFunction;
 import org.apache.spark.sql.Dataset;
@@ -11,13 +12,8 @@ public class JoinWithMinedAsset implements BiFunction<Dataset<Row>, Dataset<Row>
 
   @Override
   public Dataset<Row> apply(Dataset<Row> targetDS, Dataset<Row> assetDS) {
-    targetDS.show();
-
-    assetDS.show();
-    //@TODO: Left off here, need to clean this up
-    final Dataset<Row> day = targetDS.join(assetDS, targetDS.col("Day").equalTo(assetDS.col(RVN_CREATED_DATE)),
-        LEFT_JOIN);
-    day.show();
-    return day;
+    return targetDS.join(assetDS,
+        targetDS.col(CREATED_UNIQUE_ID).equalTo(assetDS.col(RVN_CREATED_UNIQUE_ID)),
+        OUTER_JOIN);
   }
 }
