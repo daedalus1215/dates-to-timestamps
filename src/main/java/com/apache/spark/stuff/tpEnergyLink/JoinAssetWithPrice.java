972d113d899c218b733d32dbb6503187c8b0cf85
larry adams <ladams1776@protonmail.com>
2021-08-01 17:03:08 -0400

    Add code to join for asset and price

--- src/main/java/com/apache/spark/stuff/tpEnergyLink/JoinAssetWithPrice.java
+++ src/main/java/com/apache/spark/stuff/tpEnergyLink/JoinAssetWithPrice.java
@@ -1,5 +1,19 @@
 package com.apache.spark.stuff.tpEnergyLink;
 
-public class JoinAssetWithPrice {
+import static com.apache.spark.stuff.tpEnergyLink.Constants.LEFT_JOIN;
+import static com.apache.spark.stuff.tpEnergyLink.Constants.PRICE_CREATED_DATE;
+import static com.apache.spark.stuff.tpEnergyLink.Constants.RVN_CREATED_DATE;
 
+import java.util.function.BiFunction;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+
+public class JoinAssetWithPrice implements BiFunction<Dataset<Row>, Dataset<Row>, Dataset<Row>> {
+
+  @Override
+  public Dataset<Row> apply(Dataset<Row> targetDS, Dataset<Row> assetDS) {
+    return targetDS.join(assetDS,
+        targetDS.col(RVN_CREATED_DATE).equalTo(assetDS.col(PRICE_CREATED_DATE)),
+        LEFT_JOIN);
+  }
 }
