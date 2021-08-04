dbf6c7dc667adf5ae38b1815857b1c89c2d10abb
larry adams <ladams1776@protonmail.com>
2021-08-04 16:06:22 -0400

    TpEnergyLink related change - Update the App consumption

--- src/main/java/com/apache/spark/stuff/tpEnergyLink/TpEnergyLinkDayToDayApp.java
+++ src/main/java/com/apache/spark/stuff/tpEnergyLink/TpEnergyLinkDayToDayApp.java
@@ -2,7 +2,6 @@ package com.apache.spark.stuff.tpEnergyLink;
 
 import static com.apache.spark.stuff.tpEnergyLink.Constants.CREATED_DATE;
 import static com.apache.spark.stuff.tpEnergyLink.Constants.CREATED_DAY;
-import static com.apache.spark.stuff.tpEnergyLink.Constants.CREATED_FILE_NAME_AND_PARENT_DIR_TEMP;
 import static com.apache.spark.stuff.tpEnergyLink.Constants.CREATED_FILE_NAME_AND_PREFIX_TEMP;
 import static com.apache.spark.stuff.tpEnergyLink.Constants.CREATED_ID;
 import static com.apache.spark.stuff.tpEnergyLink.Constants.CREATED_MONTH;
@@ -11,18 +10,20 @@ import static com.apache.spark.stuff.tpEnergyLink.Constants.CREATED_SUM_WATTS_FO
 import static com.apache.spark.stuff.tpEnergyLink.Constants.CREATED_UNIQUE_ID;
 import static com.apache.spark.stuff.tpEnergyLink.Constants.CREATED_WATTS;
 import static com.apache.spark.stuff.tpEnergyLink.Constants.CREATED_YEAR;
-import static com.apache.spark.stuff.tpEnergyLink.Constants.PRICE_CREATED_DATE;
-import static com.apache.spark.stuff.tpEnergyLink.Constants.PRICE_SOURCE_DATE;
 import static com.apache.spark.stuff.tpEnergyLink.Constants.PRICE_SOURCE_PRICE;
-import static com.apache.spark.stuff.tpEnergyLink.Constants.RVN_CREATED_COUNT_OF_RECORDS_A_DAY;
-import static com.apache.spark.stuff.tpEnergyLink.Constants.RVN_CREATED_DATE;
 import static com.apache.spark.stuff.tpEnergyLink.Constants.RVN_CREATED_SUM_OF_A_DAY;
-import static com.apache.spark.stuff.tpEnergyLink.Constants.RVN_CREATED_UNIQUE_ID;
-import static com.apache.spark.stuff.tpEnergyLink.Constants.RVN_SOURCE_AMOUNT;
+import static com.apache.spark.stuff.tpEnergyLink.Constants.RVN_SOURCE_ADDRESS;
+import static com.apache.spark.stuff.tpEnergyLink.Constants.RVN_SOURCE_AMOUNT_ASSET;
+import static com.apache.spark.stuff.tpEnergyLink.Constants.RVN_SOURCE_AMOUNT_RIG_ID;
+import static com.apache.spark.stuff.tpEnergyLink.Constants.RVN_SOURCE_AMOUNT_RVN;
+import static com.apache.spark.stuff.tpEnergyLink.Constants.RVN_SOURCE_AMOUNT_TRANSACTION_ID;
+import static com.apache.spark.stuff.tpEnergyLink.Constants.RVN_SOURCE_CONFIRMED;
 import static com.apache.spark.stuff.tpEnergyLink.Constants.RVN_SOURCE_DATE;
-import static com.apache.spark.stuff.tpEnergyLink.Constants.RVN_SOURCE_ID;
+import static com.apache.spark.stuff.tpEnergyLink.Constants.RVN_SOURCE_LABEL;
+import static com.apache.spark.stuff.tpEnergyLink.Constants.RVN_SOURCE_TYPE;
 import static com.apache.spark.stuff.tpEnergyLink.Constants.SOURCE_POWER;
 import static com.apache.spark.stuff.tpEnergyLink.Constants.SOURCE_UNIX_TIMESTAMP;
+import static java.util.Arrays.asList;
 import static org.apache.spark.sql.functions.avg;
 import static org.apache.spark.sql.functions.col;
 import static org.apache.spark.sql.functions.concat;
@@ -36,12 +37,17 @@ import static org.apache.spark.sql.functions.sum;
 import static org.apache.spark.sql.functions.to_date;
 import static org.apache.spark.sql.functions.year;
 
-import com.apache.spark.stuff.tpEnergyLink.transformers.PlugIdAndNameTransformer;
-import com.apache.spark.stuff.functions.writers.CsvWriter;
+import com.apache.spark.stuff.SparkPipeline;
+import com.apache.spark.stuff.functions.GetSparkSession;
 import com.apache.spark.stuff.functions.readers.GetDatasetFromCsv;
 import com.apache.spark.stuff.functions.readers.GetDatasetFromJsonMultilineRecursive;
-import com.apache.spark.stuff.functions.GetSparkSession;
+import com.apache.spark.stuff.functions.writers.CsvWriter;
 import com.apache.spark.stuff.functions.writers.WriterInterface;
+import com.apache.spark.stuff.tpEnergyLink.rvn.mining.transformers.CreatedSumOfDayAggregation;
+import com.apache.spark.stuff.tpEnergyLink.rvn.mining.transformers.CreatedUniqueId;
+import com.apache.spark.stuff.tpEnergyLink.rvn.mining.transformers.MiningCreatedDate;
+import com.apache.spark.stuff.tpEnergyLink.rvn.prices.transformers.PricesCreatedDate;
+import com.apache.spark.stuff.tpEnergyLink.transformers.PluginIdFromSourceFile;
 import com.sun.prism.PixelFormat.DataType;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
@@ -67,49 +73,36 @@ public class TpEnergyLinkDayToDayApp {
     final String pathToFiles4 = args[3];
 
     // Declare everything
-    final GetIdField getIdField = new GetIdField();
+//    final PluginIdFromSourceFile pluginIdFromSourceFile = new PluginIdFromSourceFile(absolutePathToSourceDirectory);
     final JoinWithPluginIdAndName joinWithPluginIdAndName = new JoinWithPluginIdAndName();
-    final GetDatasetFromCsv getDatasetFromCsv = new GetDatasetFromCsv();
+//    final GetDatasetFromCsv getDatasetFromCsv = new GetDatasetFromCsv();
     final JoinAssetWithPrice joinAssetWithPrice = new JoinAssetWithPrice();
 
     final GetSparkSession getSparkSession = new GetSparkSession();
-    final GetDatasetFromJsonMultilineRecursive getDatasetFromJsonMultilineRecursive = new GetDatasetFromJsonMultilineRecursive();
+//    final GetDatasetFromJsonMultilineRecursive getDatasetFromJsonMultilineRecursive = new GetDatasetFromJsonMultilineRecursive();
     final WriterInterface writer = new CsvWriter();
     final JoinWithMinedAsset joinWithMinedAsset = new JoinWithMinedAsset();
 
     // Setup
     final SparkSession sparkSession = getSparkSession.get();
-    final PlugIdAndNameTransformer plugIdAndNameTransformer = new PlugIdAndNameTransformer(sparkSession);
+    final PlugIdAndNameReader pluginIdAndNameDS = new PlugIdAndNameReader(sparkSession);
 
-    // Load the files
-    final Dataset<Row> pluginIdAndNameDS = plugIdAndNameTransformer.get();
-    final Dataset<Row> ravenDS = getDatasetFromCsv.apply(sparkSession, pathToRvnLog)
-        .withColumn(RVN_CREATED_DATE, to_date(col(RVN_SOURCE_DATE)))
-        .withColumn(RVN_CREATED_UNIQUE_ID, concat(col(RVN_CREATED_DATE), lit('_'), col(RVN_SOURCE_ID)))
-        .groupBy(col(RVN_CREATED_DATE), col(RVN_SOURCE_ID), col(RVN_CREATED_UNIQUE_ID))
-        .agg(
-            count(lit(1)).alias(RVN_CREATED_COUNT_OF_RECORDS_A_DAY),
-            sum(col(RVN_SOURCE_AMOUNT)).alias(RVN_CREATED_SUM_OF_A_DAY))
-        .sort(col(RVN_CREATED_DATE));
-    final Dataset<Row> plugin2 = getDatasetFromJsonMultilineRecursive.apply(sparkSession, pathToFiles2);
-    final Dataset<Row> plugin3 = getDatasetFromJsonMultilineRecursive.apply(sparkSession, pathToFiles3);
-    final Dataset<Row> priceOfRvn = getDatasetFromCsv.apply(sparkSession, pathToFiles4);
-
-    // Process files
-    final Dataset<Row> plug2 = getIdField.apply(plugin2, pathToFiles2);
-    final Dataset<Row> plug3 = getIdField.apply(plugin3, pathToFiles3);
-
-    final Dataset<Row> union = plug2.union(plug3);
-
-    final Dataset<Row> withDatesAndAggregations = union
-        .withColumn(CREATED_DATE, date_format(col(SOURCE_UNIX_TIMESTAMP)
-            .divide(1000).cast(DataTypes.TimestampType), "yyyy-MM-dd HH:mm:ss"))
+    // Energy
+    final Dataset<Row> withWatts = (new SparkPipeline(sparkSession)
+        .withReader(new GetDatasetFromJsonMultilineRecursive(pathToFiles2))
+        .withTransformers(asList(new PluginIdFromSourceFile(pathToFiles2)))
+        .withRemovableColumns(CREATED_FILE_NAME_AND_PREFIX_TEMP, CREATED_REMOVED_PATH_TO_FILE_TEMP)
+        .build()
+    ).union(new SparkPipeline(sparkSession)
+        .withReader(new GetDatasetFromJsonMultilineRecursive(pathToFiles3))
+        .withTransformers(asList(new PluginIdFromSourceFile(pathToFiles3)))
+        .withRemovableColumns(CREATED_FILE_NAME_AND_PREFIX_TEMP, CREATED_REMOVED_PATH_TO_FILE_TEMP)
+        .build()).withColumn(CREATED_DATE, date_format(col(SOURCE_UNIX_TIMESTAMP)
+        .divide(1000).cast(DataTypes.TimestampType), "yyyy-MM-dd HH:mm:ss"))
         .withColumn(CREATED_DAY, to_date(col(CREATED_DATE)))
         .withColumn(CREATED_MONTH, month(col(CREATED_DATE)))
         .withColumn(CREATED_YEAR, year(col(CREATED_DATE)))
-        .withColumn(CREATED_UNIQUE_ID, concat(col(CREATED_DAY), lit('_'), col(CREATED_ID)));
-
-    final Dataset<Row> withWatts = withDatesAndAggregations
+        .withColumn(CREATED_UNIQUE_ID, concat(col(CREATED_DAY), lit('_'), col(CREATED_ID)))
         .withColumn(CREATED_WATTS, col(SOURCE_POWER).cast(String.valueOf(DataType.INT)))
         .groupBy(col(CREATED_DAY), col(CREATED_ID), col(CREATED_UNIQUE_ID))
         .agg(
@@ -121,29 +114,29 @@ public class TpEnergyLinkDayToDayApp {
         .sort(col(CREATED_DAY)
         );
 
-    final Dataset<Row> assetWithDate = priceOfRvn.withColumn(PRICE_CREATED_DATE, to_date(col(PRICE_SOURCE_DATE)));
-    final Dataset<Row> withAsset = joinWithMinedAsset.apply(withWatts, joinAssetWithPrice.apply(ravenDS, assetWithDate));
-    final Dataset<Row> d = withAsset.withColumn("PRICE_ASSET_TOTAL_VALUE", col(PRICE_SOURCE_PRICE).multiply(col(RVN_CREATED_SUM_OF_A_DAY)));
+    // Raven
+    final Dataset<Row> assetWithDate = new SparkPipeline(sparkSession)
+        .withReader(new GetDatasetFromCsv(pathToFiles4))
+        .withTransformers(asList(new PricesCreatedDate()))
+        .build();
 
-    final Dataset<Row> withMathDone = d
+    final Dataset<Row> ravenMiningDS = new SparkPipeline(sparkSession)
+        .withReader(new GetDatasetFromCsv(pathToRvnLog))
+        .withRemovableColumns(RVN_SOURCE_CONFIRMED, RVN_SOURCE_DATE, RVN_SOURCE_TYPE, RVN_SOURCE_LABEL, RVN_SOURCE_ADDRESS,
+            RVN_SOURCE_AMOUNT_RVN, RVN_SOURCE_AMOUNT_ASSET, RVN_SOURCE_AMOUNT_TRANSACTION_ID, RVN_SOURCE_AMOUNT_RIG_ID)
+        .withTransformers(asList(new MiningCreatedDate(), new CreatedUniqueId(), new CreatedSumOfDayAggregation()))
+        .build();
+
+    //@TODO: DONE ABOVE
+    final Dataset<Row> withAsset = joinWithMinedAsset.apply(withWatts, joinAssetWithPrice.apply(ravenMiningDS, assetWithDate));
+    final Dataset<Row> withMathDone = withAsset.withColumn("PRICE_ASSET_TOTAL_VALUE", col(PRICE_SOURCE_PRICE).multiply(col(RVN_CREATED_SUM_OF_A_DAY)))
         .withColumn("Real Summed Watts for Day and plug", col(CREATED_SUM_WATTS_FOR_DAY_AND_PLUG_TEMP).divide(col("Number of Log Entries")))
         .withColumn("Energy Cost for Day", col("Real Summed Watts for Day and plug").divide(1000).multiply(0.07).multiply(24))
         .withColumn("Watt Hours a day", col("Real Summed Watts for Day and plug").multiply(24));
 
-    final Dataset<Row> joinedWithName = joinWithPluginIdAndName.apply(withMathDone, pluginIdAndNameDS);
+    final Dataset<Row> joinedWithName = joinWithPluginIdAndName.apply(withMathDone, pluginIdAndNameDS.get());
 
-    final Dataset<Row> cleanedUp = joinedWithName.drop(CREATED_SUM_WATTS_FOR_DAY_AND_PLUG_TEMP,
-        CREATED_FILE_NAME_AND_PREFIX_TEMP,
-        CREATED_FILE_NAME_AND_PARENT_DIR_TEMP,
-        CREATED_ID,
-        CREATED_REMOVED_PATH_TO_FILE_TEMP,
-        PRICE_SOURCE_DATE,
-        "market_value",
-        "total_volume",
-        "market_cap",
-        RVN_CREATED_UNIQUE_ID);
-
-    writer.accept(cleanedUp.repartition(1), "daily");
+    writer.accept(joinedWithName.repartition(1), "daily");
 
     sparkSession.close();
   }
